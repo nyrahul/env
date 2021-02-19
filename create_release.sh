b985804bb620222ac44e1601aaa278fc91cc1535
Rahul Jadhav <nyrahul@gmail.com>
2021-02-19 18:28:47 +0530

    updates

--- create_release.sh
+++ create_release.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-if ! command -v gh-release-create &> /dev/null; then
+if ! command -v gh &> /dev/null; then
 	echo "gh (GitHub) cli not found"
+	exit 1
+fi
+
+if [ ! -z "$(git status --untracked-files=no --porcelain)" ]; then
+	git status --untracked-files=no --porcelain
+	echo "Above uncommitted changes in local repo"
 	exit 1
 fi
