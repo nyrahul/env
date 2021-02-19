c2b1d02843c03598a498bbea648adbde7e917a9a
Rahul Jadhav <nyrahul@gmail.com>
2021-02-19 18:46:19 +0530

    updates

--- create_release.sh
+++ create_release.sh
@@ -6,8 +6,22 @@ if ! command -v gh &> /dev/null; then
 fi
 
 if ! git diff-index --quiet HEAD; then
-#if [ ! -z "$(git status --untracked-files=no --porcelain)" ]; then
 	git status --untracked-files=no --porcelain
 	echo "Above uncommitted changes in local repo"
 	exit 1
 fi
+
+[[ "$1" == "" ]] && echo "Need release number in the form of vMAJOR.MINOR.PATCH" && exit 1
+
+if ! git rev-parse "$1" >/dev/null 2>&1; then
+	echo "Creating new tag $1"
+	if ! git tag $1; then
+		echo "Could not create tag $1"
+		exit 1
+	fi
+else
+	echo "Found existing tag $1"
+fi
+
+gh release create $1 /tmp/t.tgz --draft=true --notes-file=/home/rahul/env/README.md -t "Env release $1"
+
