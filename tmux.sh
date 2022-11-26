9a7c8d575e857215fe9d6483a5a1dda6f6af5b84
Rahul Jadhav <nyrahul@gmail.com>
2022-11-26 20:02:05 +0530

    tmux sh
    Signed-off-by: Rahul Jadhav <nyrahul@gmail.com>

---
+++ tmux.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+install()
+{
+    hash $1 2>/dev/null
+    [[ $? -eq 0 ]] && echo "${2-$1} already installed" && return
+    sudo apt -y install ${2-$1}
+}
+
+install git
+install tmux
+git clone https://github.com/gpakosz/.tmux.git ~/
+ln -s -f $HOME/.tmux/.tmux.conf $HOME/.tmux.conf
+curl https://raw.githubusercontent.com/nyrahul/env/master/tmux.conf.local -o $HOME/
